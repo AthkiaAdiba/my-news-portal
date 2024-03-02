26841d1b0468cccabdd83c10fa34637e3b484114
thkiaAdiba <athkiaadiba@gmail.com>
2024-03-02 12:04:35 +0600

    added functional works

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-let selectedCategory = 1;
+
 
 
 const buttonByCategory = async () => {
@@ -23,7 +23,12 @@ const buttonByCategory = async () => {
 }
 
 
+
+
+
+
 const showCardByCategoryId = async(categoryId) => {
+  document.getElementById('loading-spiner').classList.add('hidden')
     console.log(categoryId);
     selectedCategory = categoryId;
     const res = await fetch(` https://openapi.programming-hero.com/api/news/category/${categoryId}`)
@@ -31,35 +36,29 @@ const showCardByCategoryId = async(categoryId) => {
     const allCard = data.data;
     console.log(allCard);
 
-
     const cardContainer = document.getElementById('news-container');
 
     cardContainer.innerHTML = ''
     allCard.forEach(card => {
+      document.getElementById('loading-spiner').classList.remove('hidden')
         console.log(card)
 
         
         const newsCard = document.createElement('div');
         newsCard.innerHTML = `
         <div class="card card-side bg-base-100 shadow-xl p-3 pr-5">
-      <figure><img class="h-full w-[244px]" src="${card.
+      <figure><img class="h-full w-[244px] flex-1" src="${card.
         thumbnail_url}"
           alt="Movie" /></figure>
       <div class="card-body">
         <div class="flex justify-between items-center">
-          <h2 class="card-title text-[#121221] text-2xl font-bold">The best fashion influencers to follow for sartorial inspiration</h2>
+          <h2 class="card-title text-[#121221] text-2xl font-bold">${card.title}</h2>
           <div class="flex items-center gap-3">
             <p class="text-[#121221] text-lg font-bold">${card.rating.number}</p>
             <p class="text-[#121221] text-lg font-bold">${card.rating.badge}</p>
           </div>
         </div>
-        <p>There’s no better time than now to familiarise yourself with the best online vintage clothing stores. If you
-          want to <br> overhaul your wardrobe for the long run, mixing vintage with high street clothing is the key to
-          being trendy as well as sustainable.
-          <br>
-          <br>
-          But vintage shopping isn’t easy, you can easily spend hours in a store and walk out with...
-        </p>
+        <p>${card.details.slice(0, 200)}</p>
         <!-- profile div -->
         <div class="flex items-center justify-between">
           <div class="flex items-center gap-3">
@@ -75,7 +74,7 @@ const showCardByCategoryId = async(categoryId) => {
           </div>
           <div class="flex items-center">
             <i class="fa-regular fa-eye"></i>
-            <p class="text-[#515151] text-lg font-bold"><span>1.5</span>M</p>
+            <p class="text-[#515151] text-lg font-bold"><span>${card?.total_view}</span></p>
           </div>
           <div class="rating">
             <input type="radio" name="rating-2" class="mask mask-star-2 bg-[#515151]" />
@@ -95,6 +94,18 @@ const showCardByCategoryId = async(categoryId) => {
     })
 }
 
-showCardByCategoryId(selectedCategory);
+
+const handleSearch = () => {
+  const value = document.getElementById('search-box').value;
+
+  if(value){
+    showCardByCategoryId(value);
+  }
+  else{
+    alert("Please Enter a Category Id")
+  }
+}
+
+showCardByCategoryId('08');
 
 buttonByCategory();
